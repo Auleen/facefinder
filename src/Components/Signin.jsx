4bef8f0822452f6589045efa4d24ee1803eddd34
Pious <auleenghosh.sg@gmail.com>
2022-06-24 21:07:18 +0530

    Signin Added

---
+++ src/Components/Signin.jsx
@@ -0,0 +1,42 @@
+
+import {Routes, Route, useNavigate,Link} from 'react-router-dom'
+import React, { useState } from "react"
+import './Signin.css'
+export default function (props) {
+  const navigate = useNavigate();
+    return (
+      <div className="Auth-form-container">
+        <form className="Auth-form">
+          <div className="Auth-form-content">
+            <h3 className="Auth-form-title">Sign In</h3>
+            <div className="form-group mt-3">
+              <label>Email address</label>
+              <input
+                type="email"
+                className="form-control mt-1"
+                placeholder="Enter email"
+              />
+            </div>
+            <div className="form-group mt-3">
+              <label>Password</label>
+              <input
+                type="password"
+                className="form-control mt-1"
+                placeholder="Enter password"
+              />
+            </div>
+            <div className="d-grid gap-2 mt-3">
+          <Link className="btn btn primary" to="/home" state={"100"}>
+        Checkout
+      </Link>
+            </div>
+            <p className="text-center mt-2">
+              Forgot <a href="#">password?</a>
+            </p>
+          </div>
+        </form>
+      </div>
+    )
+  }
+
+
